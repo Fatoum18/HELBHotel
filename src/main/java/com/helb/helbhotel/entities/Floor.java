0d01379d20f74574d900feac32cf32913bc0799a
TTSGeek <tsausthengeek@gmail.com>
2025-04-06 03:44:46 -0400

    Add lombok helper

---
+++ src/main/java/com/helb/helbhotel/entities/Floor.java
@@ -0,0 +1,12 @@
+package com.helb.helbhotel.entities;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Floor {
+    @Getter @Setter
+    private String name;
+
+    private  String address;
+
+    private int starRating;
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.helb.helbhotel {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires lombok;
 
     opens com.helb.helbhotel to javafx.fxml;
     exports com.helb.helbhotel;
