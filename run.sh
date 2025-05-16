bce9ab26e78bbcf726acc1d982ce14134e690a51
TTSGeek <tsausthengeek@gmail.com>
2025-05-16 11:29:29 -0400

    fix run.sh

--- run.sh
+++ run.sh
@@ -6,29 +6,31 @@ if [ ! -f "pom.xml" ]; then
     exit 1
 fi
 
-
 # Check if Maven Wrapper is installed
 if [ -f "./mvnw" ]; then
     # Make sure mvnw is executable
     chmod +x ./mvnw
     MVN_CMD="./mvnw"
-    echo "Maven Wrapper was founded in the project"
+    echo "Maven Wrapper was found in the project"
 else
-    echo "Maven is not installed. Please install Maven and try again."
+    echo "Maven Wrapper not found. Please install it or use a system Maven."
     exit 1
 fi
 
- 
-# Clean and package the project
-echo "Building the JavaFX application..."
-$MVN_CMD clean package -Dmaven.compiler.forceJavacCompilerUse=true || { echo "Failed to build the application"; exit 1; }
+# Clean and package the project without running tests
+echo "Building the JavaFX application (tests will be skipped)..."
+$MVN_CMD clean package -DskipTests -Dmaven.compiler.forceJavacCompilerUse=true || {
+    echo "Failed to build the application";
+    exit 1;
+}
 
 echo "Build done"
 
-
+# Run the application
 echo "Running the application..."
-$MVN_CMD javafx:run  || { echo "Failed to run application"; exit 1; }
+$MVN_CMD javafx:run || {
+    echo "Failed to run application";
+    exit 1;
+}
 
 echo "Application execution completed."
- 
-
